1b10db4f0168f568af57aca646c38cfe9168248f
Khrystyna Romanyshyn <Khrystyna_Romanyshyn@epam.com>
2018-01-23 17:47:38 +0200

    doesnt work dropdown menu

---
+++ MSAutoTest/MSAutoTest/SpecFlowFeature1.feature
@@ -0,0 +1,13 @@
+﻿Feature: SpecFlowFeature1
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@mytag
+Scenario: The "Contacts" page exists.
+	Given I am on the Home page
+	| Login | Password |
+	| admin | admin    | 
+	When About Us menu item is clicked
+    And Contacts submenu item is clicked
+	Then Contacts page opens
---
+++ MSAutoTest/MSAutoTest/Tests.cs
@@ -0,0 +1,57 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using TechTalk.SpecFlow;
+
+namespace MSAutoTest
+{
+
+    [Binding]
+
+    public class Tests
+    {
+        public IWebDriver driver = new ChromeDriver();
+       
+        [Given(@"I am on the Home page")]
+
+        public void GivenIAmOnTheHomePage_(Table table)
+        {
+            driver.Navigate().GoToUrl("http://ecsc00106bdc.epam.com:4200/");
+            driver.FindElement(By.XPath("// app-user-state/a")).Click();
+
+            string login = table.Rows[0]["Login"];
+            driver.FindElement(By.CssSelector(" div:nth-child(1) > input")).SendKeys(login);
+
+            string password = table.Rows[0]["Password"];
+            driver.FindElement(By.CssSelector(" div:nth-child(2) > input")).SendKeys(password);
+
+            driver.FindElement(By.CssSelector(".login-form__submit-container")).Submit();
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+        }
+
+        [When(@"About Us menu item is clicked")]
+
+        public void WhenAboutUsMenuItemIsClicked()
+        {
+            driver.FindElement(By.CssSelector("li:nth-child(1) > a")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.CssSelector("li:nth-child(4) > a")).Click();
+        }
+
+        [When(@"Contacts submenu item is clicked")]
+
+        public void WhenContactsSubmenuItemIsClicked_()
+        {
+            
+        }
+
+        [Then(@"Contacts page opens")]
+
+        public void ThenContactsPageOpens_()
+        {
+            
+        }
+
+    }
+}
