1a7dd447eee5f916c7b2cfe060a9349e6fa000ea
Khrystyna Romanyshyn <Khrystyna_Romanyshyn@epam.com>
2018-02-02 13:48:19 +0200

    add file with test from portableTest

---
+++ Test/Test/Class1.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+using System;
+
+using TechTalk.SpecFlow;
+
+namespace Test
+{
+    [Binding]
+    public class Test1Steps
+    {
+        public IWebDriver driver = new ChromeDriver();
+
+        [Given(@"the main page with credentials is opened")]
+        public void GivenTheMainPageWithCredentialsIsOpened()
+        {
+            driver.Navigate().GoToUrl("http://ecsc00105d83.epam.com:85");
+        }
+
+        [When(@"user logs in")]
+        public void WhenUserLogsIn(Table table)
+        {
+            string login = table.Rows[0]["Login"];
+            driver.FindElement(By.Id("user_email")).SendKeys(login);
+            IWebElement button_login = driver.FindElement(By.XPath("//button[contains(text(), 'Enter')]"));
+            button_login.Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            string password = table.Rows[0]["Password"];
+            driver.FindElement(By.CssSelector("#user_password")).SendKeys(password);
+            driver.FindElement(By.XPath("//button[contains(text(), 'Enter')]")).Click();
+           driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+        }
+
+        [Then(@"the user's main page opens")]
+        public void ThenTheUserSMainPageOpens()
+        {
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            bool isElementPresent = driver.FindElement(By.XPath("//button[contains(text(), 'Logout')]")).Enabled;
+            Assert.AreEqual(true, isElementPresent);    
+        }
+    }
+}
---
+++ Test/Test/SpecFlowFeature1.feature
@@ -0,0 +1,9 @@
+﻿Feature: Test 1
+
+@mytag
+Scenario: Login as admin
+	Given the main page with credentials is opened
+	When user logs in 
+	| Login          | Password |
+	| admin@epam.com | admin123 | 
+	Then the user's main page opens
